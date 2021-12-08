398500a4d3a07e34136769d4b345f246473885a5
郎 秀晨 <22920192204222@stu.xmu.edu.cn>
2021-12-08 10:46:32 +0000

    Update daily-test.sh

--- daily-test.sh
+++ daily-test.sh
@@ -1,21 +1,21 @@
 #|bin/bash
 ## 将文件结尾从CRLF改为LF，解决了cd 错误问题
 workdir_oomall=/home/lxc/project/oomall
-workdir_privilege=/home/lxc/project/privilege
+workdir_privilege=/home/lxc/project/privilegegateway
 workdir_ooad201=/home/lxc/project/ooad201
 testdir=/home/testPort
 time=$(date "+%Y-%m-%d--%H:%M:%S")
-
-echo '-------------------install core--------------------------'
-cd $workdir_oomall
-git pull
-cd $workdir_oomall/core
-mvn clean install >> /home/console.log
 
 echo '-------------------install annotaion--------------------------'
 cd $workdir_privilege
 git pull
 cd $workdir_privilege/annotation
+mvn clean install >> /home/console.log
+
+echo '-------------------install core--------------------------'
+cd $workdir_oomall
+git pull
+cd $workdir_oomall/core
 mvn clean install >> /home/console.log
 
 echo '-------------------initializing ooad database-------------------------'
