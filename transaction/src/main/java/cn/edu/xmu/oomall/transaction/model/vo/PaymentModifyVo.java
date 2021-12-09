543764a70c88a9f8099282e6f566e9aecb77b1cd
YitongGao1016 <2496241579@qq.com>
2021-12-09 22:11:12 +0800

    b-1

---
+++ transaction/src/main/java/cn/edu/xmu/oomall/transaction/model/vo/PaymentModifyVo.java
@@ -0,0 +1,13 @@
+package cn.edu.xmu.oomall.transaction.model.vo;
+
+import lombok.Data;
+
+/**
+ * @author 高艺桐 22920192204199
+ * @date 2021/12/09 21:00
+ */
+@Data
+public class PaymentModifyVo {
+    private Byte state;
+    private String descr;
+}
