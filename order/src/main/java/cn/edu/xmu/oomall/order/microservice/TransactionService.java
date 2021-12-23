043df8b8a0ce2c57705f884f3b5615c6288d4dff
1114796814 <1114796814@qq.com>
2021-12-23 20:50:33 +0800

    监听者service 时间格式

--- order/src/main/java/cn/edu/xmu/oomall/order/microservice/TransactionService.java
+++ order/src/main/java/cn/edu/xmu/oomall/order/microservice/TransactionService.java
@@ -23,35 +23,38 @@ import java.util.Map;
  * @author 高艺桐 22920192204199
  * @date 2021/12/09 16:32
  */
-@FeignClient(name = "transaction-service",configuration= OpenFeignConfig.class)
+@FeignClient(name = "transaction-service", configuration = OpenFeignConfig.class)
 public interface TransactionService {
-//TODO:GYT
+    //TODO:GYT
     @GetMapping("/shops/{shopId}/payment")
     InternalReturnObject<PageVo<PaymentRetVo>> listPayment(@PathVariable(value = "shopId") Long shopId,
-                                                         @RequestParam(value = "documentId",required = false)String documentId,
-                                                         @RequestParam(value = "state",required = false)Byte state,
-                                                         @RequestParam(value = "beginTime",required = false)@DateTimeFormat(pattern = MyDateTime.DATE_TIME_FORMAT) LocalDateTime beginTime,
-                                                         @RequestParam(value = "endTime",required = false)@DateTimeFormat(pattern = MyDateTime.DATE_TIME_FORMAT)LocalDateTime endTime,
-                                                         @RequestParam(value="page",required = false)Integer page,
-                                                         @RequestParam(value = "pageSize",required = false)Integer pageSize);
-//TODO:HTY
+                                                           @RequestParam(value = "documentId", required = false) String documentId,
+                                                           @RequestParam(value = "state", required = false) Byte state,
+                                                           @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam(value = "beginTime", required = false) ZonedDateTime beginTime,
+                                                           @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam(value = "endTime", required = false) ZonedDateTime endTime,
+                                                           @RequestParam(value = "page", required = false) Integer page,
+                                                           @RequestParam(value = "pageSize", required = false) Integer pageSize);
+
+    //TODO:HTY
     @GetMapping("/shops/{shopId}/refund")
     InternalReturnObject<PageVo<RefundRetVo>> listRefund(@PathVariable("shopId") Long shopId,
-                                                         @RequestParam(value="documentId",required = false)String documentId,
-                                                        @RequestParam(value="state",required = false)Byte state,
-                                                        @RequestParam(value = "beginTime",required = false)@DateTimeFormat(pattern = MyDateTime.DATE_TIME_FORMAT) LocalDateTime beginTime,
-                                                        @RequestParam(value = "endTime",required = false)@DateTimeFormat(pattern = MyDateTime.DATE_TIME_FORMAT)LocalDateTime endTime,
-                                                        @RequestParam(value = "page", required = false) Integer page,
-                                                        @RequestParam(value = "pageSize", required = false) Integer pageSize);
-//SOLVED BY HTY
-    @GetMapping("/internal/payment")
-    InternalReturnObject<PageVo<PaymentRetVo>> listPaymentInternal(@RequestParam(value="documentId",required = false)String documentId,
-                                                                  @RequestParam(value="state",required = false)Byte state,
-                                                         @RequestParam(value = "beginTime",required = false)@DateTimeFormat(pattern = MyDateTime.DATE_TIME_FORMAT) LocalDateTime beginTime,
-                                                         @RequestParam(value = "endTime",required = false)@DateTimeFormat(pattern = MyDateTime.DATE_TIME_FORMAT)LocalDateTime endTime,
+                                                         @RequestParam(value = "documentId", required = false) String documentId,
+                                                         @RequestParam(value = "state", required = false) Byte state,
+                                                         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam(value = "beginTime", required = false) ZonedDateTime beginTime,
+                                                         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam(value = "endTime", required = false) ZonedDateTime endTime,
                                                          @RequestParam(value = "page", required = false) Integer page,
                                                          @RequestParam(value = "pageSize", required = false) Integer pageSize);
-//TODO:GYT
+
+    //SOLVED BY HTY
+    @GetMapping("/internal/payment")
+    InternalReturnObject<PageVo<PaymentRetVo>> listPaymentInternal(@RequestParam(value = "documentId", required = false) String documentId,
+                                                                   @RequestParam(value = "state", required = false) Byte state,
+                                                                   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam(value = "beginTime", required = false) ZonedDateTime beginTime,
+                                                                   @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) @RequestParam(value = "endTime", required = false) ZonedDateTime endTime,
+                                                                   @RequestParam(value = "page", required = false) Integer page,
+                                                                   @RequestParam(value = "pageSize", required = false) Integer pageSize);
+
+    //TODO:GYT
     @PostMapping("/internal/refunds")
     InternalReturnObject<RefundRetVo> requestRefund(@Validated @RequestBody RefundRecVo refundVo);
 }
